16213182e2972cb969aee3f059911295ac09c1db
Janani27V <jananivaithilingam@gmail.com>
2025-12-22 18:52:06 +0530

    Add files via upload

--- c8.cpp
+++ c8.cpp
@@ -1,15 +1,4 @@
 #include<stdio.h>
 int main(){
-	scanf("%d %d %d", &a,&b,&c);
-	if(a<b)
-	printf("max:%d\n",a);
-	printf("min:%d\n",b);
+	int a
 }
-else if(b>a){
-	printf("max:%d\n",b);
-	printf("min:%d\n",a);
-}
-else{
-	printf("equal";)
-}
-return 0;
