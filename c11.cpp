41218088fc018c6bf24c2c4559d169ae9b4794be
Janani27V <jananivaithilingam@gmail.com>
2025-12-23 09:48:30 +0530

    Add files via upload

---
+++ c11.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int n){
+	for(i=0;i<n;i++){
+		scanf("%d",&num);
+		sum+=num;
+	}
+	return 0;
+}
+int main(){
+	int n;
+	scanf("%d",&n);
+	printf("%d",add(n));
+	return 0;
+}
