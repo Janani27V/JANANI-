d13614114ec80292bdf0cb5eaf0128cdf7426b98
Janani27V <jananivaithilingam@gmail.com>
2025-12-24 20:35:40 +0530

    Add files via upload

---
+++ c23.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+	int a,b,result;
+	printf("enter two numbers");
+	scanf("%d%d",&a,&b);
+	result=a+b;
+	printf("sum:%d",result);
+	return 0;
+}
