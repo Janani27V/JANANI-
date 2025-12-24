269340d4faca0a3a6e1eaa394026dc6da36dfe82
Janani27V <jananivaithilingam@gmail.com>
2025-12-24 20:37:31 +0530

    Add files via upload

---
+++ c27.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,sum=0;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++);
+	 scanf("%d",&arr[i]);
+	 sum+=arr[i];	
+}
+printf("%d/n",sum);
+printf("%2f",(float)sum/n);
+return 0;
+}
