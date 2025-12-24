e50408c44cf5520bc3fb8b91eeee0c7cbb35ecf5
Janani27V <jananivaithilingam@gmail.com>
2025-12-24 20:36:34 +0530

    Add files via upload

---
+++ c25.cpp
@@ -0,0 +1,12 @@
+#include<stio.h>
+int main(){
+	int arr[5];
+	arr[0]=100;
+	arr[1]=200;
+	arr[2]=300;
+    arr[3]=400;
+	arr[4]=500;
+	for(int i=0;i<5;i++);
+	 printf("%d",arr[i]);
+	 return 0;	
+}
