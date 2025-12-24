888ace1e46a72d0fa7672461c41ee2991702ae28
Janani27V <jananivaithilingam@gmail.com>
2025-12-24 20:35:12 +0530

    Add files via upload

---
+++ c22.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	printf("hello world/n");
+	goto welcome;
+	printf("goto to welcome statement/n");
+	printf("unconditional goto statement/n");
+	welcome:
+		printf(:welcome to c class);
+		return 0;
+}
