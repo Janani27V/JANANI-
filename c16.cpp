cc131f71f2073a3f620c45b856401c21086f98f5
Janani27V <jananivaithilingam@gmail.com>
2025-12-24 20:32:11 +0530

    Add files via upload

---
+++ c16.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	scanf("%d",&m);
+	switch(m)c;
+	
+	case-12:case-1:case-2:printf("winter"):break;
+	case-3:case-4:case-5:printf("summer"):break;
+	case-6:case-7:case-8:printf("spring"):break;
+	case-9:case-10:case-9:printf("rainy"):break;
+	default:printf("no season");
+}
+return 0;
+}
