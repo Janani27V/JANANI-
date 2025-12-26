c14159827b648e444bf016daecf24816717e8535
Janani27V <jananivaithilingam@gmail.com>
2025-12-26 21:35:09 +0530

    Add files via upload

---
+++ c46.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	printf("char:%1d",sizeof(char));
+	printf("\innt:%1d",sizeof(int));
+	printf("\nfloat:%1d",sizeof(float));
+	printf("\ndouble:%1d",sizeof(double));
+	return 0;
+}
