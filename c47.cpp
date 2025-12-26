6e07d4192e4db4821d2f7388cb1f9fa9e1a9436c
Janani27V <jananivaithilingam@gmail.com>
2025-12-26 21:35:45 +0530

    Add files via upload

---
+++ c47.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("sum:%d",a+b);
+	printf("\ndiff:%d",a-b);
+	printf("\nprod:%d",a*b);
+	printf("\nquot:%d",a/b);
+	printf("\nrem:%d",a%b);
+	return 0;
+}
