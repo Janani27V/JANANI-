981abff6a22daf5f762825362265650960f4d5da
Janani27V <jananivaithilingam@gmail.com>
2025-12-30 16:42:29 +0530

    Add files via upload

---
+++ c75.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	struct student{
+		int roll;
+		char name[20];
+		struct students;
+		scanf("%d%s%d%s%d%s",&s.roll,s.name,s.addr.city,&s addr.pin);
+		printf("%d%s%d%s",s.roll,s.name,s.addr.city,s.addr.pin);
+		return 0;
+}
