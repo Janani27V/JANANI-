76a4204f10f61b7f6dc6b341369ead39e58925b2
Janani27V <jananivaithilingam@gmail.com>
2025-12-30 16:42:07 +0530

    Add files via upload

---
+++ c74.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int fact(int);
+void main(){
+	int n,f;
+	printf("\n enter any value:");
+	scanf("%d",&n);
+	f=fact(n);
+	printf("factorial is %d",f);
+	{
+		int fact(int num);
+        printf("%d",&n);
+    }
+    return 0;
